f25c4c98dbea51352f7c68316b74457107741664
RUPAM GIRI <103890234+rupam121@users.noreply.github.com>
2022-05-14 11:15:01 +0530

    Add files via upload

---
+++ 10_SliceOperater.py
@@ -0,0 +1,19 @@
+# Accessing element through Slice Operater:
+# print(msg[ Start index:End index:Slice Jump ])
+
+msg = "Welcome to python"
+# print(msg[1:12:2]) #ecm op
+# print(msg[3:15:2]) #cm op
+# print(msg[::])     # Welcome to python
+# print(msg[::2])    # Wloet yhn
+# print(msg[:5000:]) # Welcome to python
+# print(msg[100:12:2]) # Nothing
+# print(msg[::-1])  # nohtyp ot emocleW
+# print(msg[::-2])  # nhy teolW
+# print(msg[5000:2:-1])  # nohtyp ot emoc
+# print(msg[::-+1])  # nohtyp ot emocleW
+# print(msg[-1:4:-1])  # nohtyp ot em
+# print(msg[1:-12:])  # elco
+
+
+print(msg[False+10:(True-True):-1])  # ot emocle
